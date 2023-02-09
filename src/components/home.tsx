c9ba7517c1829ac68db1ea5bc4dc589e8d170831
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-09 15:53:40 +0100

    feat:completed the user interface for header section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,17 +1,19 @@
 import Display from "../elements/Display";
 import Footer from "../elements/Footer";
 import Fruit from "../elements/Fruit";
+import Header from "../elements/Header";
 import Hero from "../elements/Hero";
 import Testimonial from "../elements/Testimonal";
 
 const Home: React.FC = () => {
   return (
     <section className="overflow-x-hidden">
+      <Header />
       <Hero />
-      <Fruit/>
-      <Testimonial/>
-      <Display/>
-      <Footer/>
+      <Fruit />
+      <Testimonial />
+      <Display />
+      <Footer />
     </section>
   );
 };
