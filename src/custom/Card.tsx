8b1cfe914b9fdcd811628406e7f03ac9762a01c5
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-09 23:03:13 +0100

    feat:added meta description, title and a README file

--- src/custom/Card.tsx
+++ src/custom/Card.tsx
@@ -10,9 +10,9 @@ const Card = ({image, heading, text}:Props)=>{
           <figure className="w-full">
           <img src={image} alt="fruit" className="lg:h-full md:h-full h-96 w-full"/>
         </figure>
-        <figcaption className="absolute top-52 md:top-96 lg:top-80 lg:pb-4 z-50 flex lg:text-center flex-col lg:w-9/12 md:w-6/12 sm:w-12/12 px-8">
+        <figcaption className="absolute top-52 md:top-96 lg:top-80 lg:pb-4 z-50 flex lg:text-center flex-col lg:w-8/12 md:w-6/12 sm:w-12/12 px-8">
           <h1 className="font-Headingfront text-Heading lg:text-3xl text-2xl font-bold lg:pb-6 pb-2 pt-2">{heading}</h1>
-          <p className="font-textFont text-Heading leading-6 lg:leading-8 lg:text-lg md:text-base text-sm pb-2">{text}</p>
+          <p className="font-textFont text-Heading leading-6 lg:leading-8 lg:text-base md:text-base text-sm pb-2">{text}</p>
         </figcaption>
         </section>
     )
