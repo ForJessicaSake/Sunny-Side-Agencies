dcb9e8fa9a6ab623edc815767ea05b02f99d8823
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-09 23:10:44 +0100

    feat:added meta description, title and a README file

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <section className="relative flex flex-col  justify-center items-center bg-Blue lg:pt-0 pt-10">
       <h1 className="font-Headingfront absolute lg:top-24 top-8 leading-10 text-4xl w-80 lg:w-full text-center lg:text-5xl text-White tracking-widest">WE ARE CREATIVES</h1>
       <img src={arrowImage} alt="orange" className="absolute lg:h-20 h-16 animate-bounce" />
-      <img src={roundedCitrus} alt="orange" className="lg:w-8/12 w-full lg:h-full h-72" />
+      <img src={roundedCitrus} alt="orange" className="lg:w-8/12 w-full lg:h-full md:h-full h-72" />
     </section>
   );
 };
