cf0db829fe39051a7bccee504b64811295a47fae
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-09 19:10:48 +0100

    feat:completed the navigation feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav className="font-textFont bg-Blue cursor-pointer flex justify-between lg:px-6 py-6 items-center">
       <h2 className="text-4xl ml-4 font-extrabold text-White">Sunnyside</h2>
       <img src ={menu} alt ="menu" className=" mr-4 w-8 h-6 lg:hidden" onClick={handleClose}/>
-      <ul className={`${state ? 'block': 'hidden'} lg:static absolute lg:top-0 top-24 lg:h-0 h-96 pb-10 lg:pb-0 flex lg:flex-row flex-col bg-Blue z-50 lg:text-sm text-base lg:flex items-center justify-between lg:w-96 w-full text-White`}>
+      <ul className={`${state ? 'block': 'hidden'} lg:static absolute lg:top-0 top-28 lg:h-0 h-96 pb-10 lg:pb-0 flex lg:flex-row flex-col bg-Blue z-50 lg:text-sm text-base lg:flex items-center justify-between lg:w-96 w-full text-White`}>
         <li>About</li>
         <li>Services</li>
         <li>Projects</li>
