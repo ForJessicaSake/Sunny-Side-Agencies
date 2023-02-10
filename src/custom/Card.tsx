52a6ae94271a7d4f7f6d2d12a098db8e92e170d0
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-10 11:04:22 +0100

    feat:fix:made changes to the size of the fruit and testimonial on smaller screens

--- src/custom/Card.tsx
+++ src/custom/Card.tsx
@@ -15,7 +15,7 @@ const Card = ({ image, heading, text }: Props) => {
         />
       </figure>
       <figcaption className="absolute top-52 md:top-96 lg:top-56 xl:top-96 z-50 flex lg:text-center flex-col xl:w-7/12 lg:w-11/12 md:w-7/12 w-12/12 px-8">
-        <h1 className="font-Headingfront lg:mt-0 md:mt-10 mt-0 xl:mt-0 text-Heading lg:text-3xl text-2xl font-bold lg:pb-6 pb-2 pt-2">
+        <h1 className="font-Headingfront lg:mt-0 md:mt-10 mt-0 xl:mt-0 text-Heading lg:text-2xl text-2xl font-bold lg:pb-6 pb-2 pt-2">
           {heading}
         </h1>
         <p className="font-textFont text-Heading leading-6 lg:leading-8 lg:text-base md:text-base text-sm pb-2">
